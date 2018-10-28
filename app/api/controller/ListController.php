ca31d25678a6d48449840d377d9338f666511a0c
Fable <278380854@qq.com>
2018-10-28 14:38:30 +0800

    ajax跨域設置

--- app/api/controller/ListController.php
+++ app/api/controller/ListController.php
@@ -10,6 +10,13 @@
 // +----------------------------------------------------------------------
 namespace app\api\controller;
 
+header('Access-Control-Allow-Origin:*');
+// 响应类型
+header('Access-Control-Allow-Methods:*');
+// 响应头设置
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+
+
 use app\portal\model\ProductQueryModel;
 use cmf\controller\HomeBaseController;
 use app\portal\model\ProductModel;
--- app/api/controller/QuerytypeController.php
+++ app/api/controller/QuerytypeController.php
@@ -4,8 +4,16 @@
  */
 namespace app\api\controller;
 
+header('Access-Control-Allow-Origin:*');
+// 响应类型
+header('Access-Control-Allow-Methods:*');
+// 响应头设置
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+
+
 use app\portal\model\QueryTypeModel;
 use app\portal\model\QueryValueModel;
+use app\portal\model\ZoneModel;
 use cmf\controller\HomeBaseController;
 use think\Db;
 
@@ -17,15 +25,16 @@ class QuerytypeController extends HomeBaseController
 
         $value_list = QueryValueModel::field('qv_id,qt_id,qv_value')->order('qv_sort','asc')->select()->toArray();
 
-
         foreach ($type_list as &$type){
             for ($i=0; $i < count($value_list);$i++){
                 if($type['qt_id'] == $value_list[$i]['qt_id']){
-                    $type['values'][] =$value_list[$i];
+                    $type['values'][] = $value_list[$i];
                 }
             }
         }
 
-        $this->apisucces('筛选条件列表',$type_list);
+        $zone_list = ZoneModel::field('z_id,z_name,z_pid')->select()->toArray();
+
+        $this->apisucces('筛选条件列表',array('query_list'=>$type_list,'zone_list'=>$zone_list));
     }
 }
