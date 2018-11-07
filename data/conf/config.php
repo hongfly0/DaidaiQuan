df189571f4c97025941591ae325a57155b61f3d9
Fable <278380854@qq.com>
2018-11-07 17:22:53 +0800

    修改配置文件

--- data/conf/config.php
+++ data/conf/config.php
@@ -7,5 +7,5 @@ return [
     'appKey'=>'CFFD4234D8C54E23CEE45C500F131367',
     'appSecret' => "ccc2e26a38e0473ab0e30c637c0d90bb",
     'accessToken' => "df42927b-6994-4ad3-8e25-e8fd0bb543b7",
-    'IMG_URL_PREFIX' => 'http://www.daidaiquan.cn/upload/'
+    'IMG_URL_PREFIX' => 'http://47.106.37.216/upload/'
 ];
