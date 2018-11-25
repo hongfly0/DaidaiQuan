d55292718ddf03b986ea9a7b040ed3f2f9b227c0
Fable <278380854@qq.com>
2018-11-25 23:25:50 +0800

    汽车列表提交

--- app/admin/controller/GoodsController.php
+++ app/admin/controller/GoodsController.php
@@ -17,7 +17,42 @@ class GoodsController extends AdminBaseController{
 
     public function goodsList()
     {
+        $content = hook_one('admin_user_index_view');
 
+        if (!empty($content)) {
+            return $content;
+        }
+
+        $where = '1=1';
+
+        /**搜索条件**/
+        $key_word = $this->request->param('key_word');
+
+        if ($key_word) {
+            $where .= " and `product_name` like '%". $key_word ."%'";
+        }
+
+        $products = Db::name('product')
+            ->where($where)
+            ->order("product_id DESC")
+            ->paginate(20);
+        // 获取分页显示
+        $page = $products->render();
+
+        $result= array();
+
+        foreach ($products as &$val){
+            $product_query = Db::name('product_query')->where('product_id',$val['product_id'])->select()->toArray();
+
+            foreach ($product_query as $row){
+                $val[$row['pt_en_name']] = $row['pv_value'];
+            }
+            $result[]= $val;
+        }
+
+        $this->assign("page", $page);
+        $this->assign("products", $result);
+        return $this->fetch();
     }
 
     public function addGoods()
