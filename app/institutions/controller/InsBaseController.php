28c3952ae485717d5edd97c58c490b7d4a2f150c
Fable <278380854@qq.com>
2019-01-26 21:53:57 +0800

    代码提交

--- app/institutions/controller/InsBaseController.php
+++ app/institutions/controller/InsBaseController.php
@@ -39,4 +39,37 @@ class InsBaseController extends BaseController
             }
         }
     }
+
+    public function _initializeView()
+    {
+        $cmfAdminThemePath    = config('cmf_admin_theme_path');
+        $cmfAdminDefaultTheme = cmf_get_current_admin_theme();
+
+        $themePath = "{$cmfAdminThemePath}{$cmfAdminDefaultTheme}";
+
+        $root = cmf_get_root();
+
+        //使cdn设置生效
+        $cdnSettings = cmf_get_option('cdn_settings');
+        if (empty($cdnSettings['cdn_static_root'])) {
+            $viewReplaceStr = [
+                '__ROOT__'     => $root,
+                '__TMPL__'     => "{$root}/{$themePath}",
+                '__STATIC__'   => "{$root}/static",
+                '__WEB_ROOT__' => $root
+            ];
+        } else {
+            $cdnStaticRoot  = rtrim($cdnSettings['cdn_static_root'], '/');
+            $viewReplaceStr = [
+                '__ROOT__'     => $root,
+                '__TMPL__'     => "{$cdnStaticRoot}/{$themePath}",
+                '__STATIC__'   => "{$cdnStaticRoot}/static",
+                '__WEB_ROOT__' => $cdnStaticRoot
+            ];
+        }
+
+        $viewReplaceStr = array_merge(config('view_replace_str'), $viewReplaceStr);
+        config('template.view_base', "$themePath/");
+        config('view_replace_str', $viewReplaceStr);
+    }
 }
